5f3fa1a4b036eda3f1c30c0eaeea48a39f6145a4
Swizec Teller <swizec@swizec.com>
2018-08-08 23:43:22 -0700

    Load and parse data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import * as d3 from "d3";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        data: null
+    };
+
+    componentDidMount() {
+        Promise.all([
+            d3.csv("/data/breed_info.csv", d => ({
+                breed: d["Breed"].toLowerCase(),
+                height: [
+                    Number(d["height_low_inches"]),
+                    Number(d["height_high_inches"])
+                ],
+                weight: [
+                    Number(d["weight_low_lbs"]),
+                    Number(d["weight_high_lbs"])
+                ]
+            })),
+            d3.csv("/data/dog_intelligence.csv", d => ({
+                breed: d["Breed"].toLowerCase(),
+                reps: [Number(d["reps_lower"]), Number(d["reps_higher"])],
+                obey: Number(d["obey"].replace("%", ""))
+            })),
+            d3.csv("/data/dog_sales.csv", d => ({
+                breed: d["Primary Breed"].toLocaleLowerCase(),
+                sales: Number(d["Num2015"])
+            }))
+        ]).then(([breeds, intelligence, sales]) => {
+            const data = d3
+                .nest()
+                .key(d => d.breed)
+                .entries([...breeds, ...intelligence, ...sales])
+                .filter(({ values }) => values.length > 1)
+                .reduce(
+                    (data, { key, values }) => ({
+                        ...data,
+                        [key]: values.reduce(
+                            (obj, entry) => ({ ...obj, ...entry }),
+                            {}
+                        )
+                    }),
+                    {}
+                );
+
+            this.setState({ data });
+        });
+    }
+
+    render() {
+        const { data } = this.state;
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    {data === null ? "Loading CSV files ..." : null}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
